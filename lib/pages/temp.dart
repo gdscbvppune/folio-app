f7a787ab17fe54521fdbf0895c070037dbdf7c25
nandinisahni <55194857+nandinisahni@users.noreply.github.com>
2020-03-26 13:49:00 +0530

    adding temp.dart

---
+++ lib/pages/temp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ class SomeScreen extends StatefulWidget {
+   @override
+   _SomeScreenState createState() => _SomeScreenState();
+ }
+ 
+ class _SomeScreenState extends State<SomeScreen> {
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       
+     );
+   }
+ }
